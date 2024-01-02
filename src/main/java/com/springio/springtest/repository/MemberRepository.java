ade0803632d825beca80bee9dfb27d39e2fb09f4
leean <zzzttt9@gmail.com>
2024-01-02 17:38:22 +0900

    [member 기능 구현]

---
+++ src/main/java/com/springio/springtest/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.springio.springtest.repository;
+
+import com.springio.springtest.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
