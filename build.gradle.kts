532a7f61409ca4fbf07030d34859d21f9ca5754c
leean <zzzttt9@gmail.com>
2024-01-02 16:25:49 +0900

    [Initial Commit] SpringTemplate

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.springio"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
